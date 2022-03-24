75a921b1129ea93c09545de540bf72a47f3963be
Lucy Marnell <lucymarnell@gmail.com>
2022-03-24 12:23:15 -0400

    print to console

--- js/visualization.js
+++ js/visualization.js
@@ -1,6 +1,4 @@
 //This is filler -- delete it and start coding your visualization tool here
-d3.select("#vis-container")
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 20)
-  .text("Hello World!");
+d3.csv("data/Cleaned_CityLife.csv").then((data) => {
+  console.log(data.slice(0, 10));
+});
