b6786e05770714d2c118a2ec177c4551ed62c905
Lucy Marnell <lucymarnell@gmail.com>
2022-04-12 12:29:41 -0400

    added heat map

--- js/visualization.js
+++ js/visualization.js
@@ -94,7 +94,7 @@ d3.csv("data/New_Cleaned_CityLife.csv").then((consdata) => {
         scatterData.push({city : cities[i], overall:overallScore[i], rating:input[i]})
     }
 
-
+/*
 //world map
 //reference:
 //https://mappingwithd3.com/getting-started/
@@ -115,6 +115,45 @@ d3.json('data/map.geo.json').then(function(bb) {
   .attr('d', geoGenerator)
   .attr('fill', '#088')
   .attr('stroke', '#000');
+});
+*/
+
+// Map and projection
+const path = d3.geoPath();
+const projection = d3.geoMercator()
+  .scale(70)
+  .center([0,20])
+  .translate([width / 2, height / 2]);
+
+// Data and color scale
+let mapData = new Map()
+const colorScale = d3.scaleLinear()
+  .domain([4.54, 5.9])
+  .range(d3.schemeBlues[3]);
+
+// Load external data and boot
+Promise.all([
+d3.json('data/continents.json'),
+d3.csv('data/dests.csv', function(d) {
+    mapData.set(d.name, +d.overallRating);
+})
+]).then(function(loadData){
+    let topo = loadData[0]
+
+// Draw the map
+  svg2.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .join("path")
+      // draw each country
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      // set the color of each country
+      .attr("fill", function (d) {
+        d.total = mapData.get(d.properties.continent) || 0;
+        return colorScale(d.total);
+      })
 });
 
 //bar chart
